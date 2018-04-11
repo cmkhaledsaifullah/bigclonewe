f24f6f2c66cfb59aa93c44bb1983a3db6882e34e
C M Khaled Saifullah <khaledkucse@gmail.com>
2018-04-11 11:20:07 -0600

    war file, toolddb, ijadataset and upload-dir added

---
+++ target/classes/static/js/scripts.js
@@ -0,0 +1,51 @@
+function fieldValidate()
+{
+	
+	var name = document.getElementById("toolname").value;
+	var description = document.getElementById("description").value;
+	if (!name.match(/\S/)) 
+	{
+		alert('Please enter a valid Tool Name.');
+		return false;
+	} 
+	else if(!description.match(/\S/)) 
+	{
+		alert('Please enter a short Decription of the tool');
+		return false;
+	}
+	else if(document.getElementById("toolFile").files.length == 0)
+	{
+		alert('Please upload the tool file');
+		return false;
+	}
+	else if(document.getElementById("toolRunnerFile").files.length == 0)
+	{
+		alert('Please upload the tool runner file');
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+
+}
+
+function detectInfoValidate()
+{
+	var output = document.getElementById("output").value;
+	var toolRunner = document.getElementById("toolRunner").value;
+	if (!toolRunner.match(/\S/)) 
+	{
+		alert('Please enter a valid Tool Runner name.');
+		return false;
+	}
+	else if (!output.match(/\S/)) 
+	{
+		alert('Please enter a valid Output file name.');
+		return false;
+	} 
+	else
+	{
+		return true;
+	}
+}
