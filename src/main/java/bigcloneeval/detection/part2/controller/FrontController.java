7cf0da0d4ba2c18691b0934cbe9648beec431c6e
C M Khaled Saifullah <khaledkucse@gmail.com>
2018-04-20 11:59:22 -0600

    Add files via upload

--- src/main/java/bigcloneeval/detection/part2/controller/FrontController.java
+++ src/main/java/bigcloneeval/detection/part2/controller/FrontController.java
@@ -1,10 +1,8 @@
 package bigcloneeval.detection.part2.controller;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -13,12 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import bigcloneeval.detection.part2.model.Tool;
 import bigcloneeval.detection.part2.service.StorageService;
